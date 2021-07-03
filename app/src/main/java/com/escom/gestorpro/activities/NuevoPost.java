aa8849e131bcbf302203210dd32a7e9a532cb3a4
sergiooli1997 <gio.lozol@outlook.com>
2021-07-03 16:49:50 -0500

    Storage_NuevoPost 1.1

--- app/src/main/java/com/escom/gestorpro/activities/NuevoPost.java
+++ app/src/main/java/com/escom/gestorpro/activities/NuevoPost.java
@@ -1,5 +1,6 @@
 package com.escom.gestorpro.activities;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,11 +9,16 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.escom.gestorpro.R;
+import com.escom.gestorpro.providers.ImageProvider;
 import com.escom.gestorpro.utils.FileUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 
@@ -22,6 +28,8 @@ public class NuevoPost extends AppCompatActivity {
     CircleImageView mCircleImageViewBack;
     ImageView mImageViewPost;
     File mImageFile;
+    Button mButtonPost;
+    ImageProvider mImagePrivder;
     private final int GALLERY_REQUEST_CODE = 1;
 
     @Override
@@ -29,8 +37,19 @@ public class NuevoPost extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_post);
 
+        mImagePrivder = new ImageProvider();
+
         mCircleImageViewBack = findViewById(R.id.circleImageBack);
         mImageViewPost = findViewById(R.id.subirImagen);
+        mButtonPost = findViewById(R.id.btnPublicar);
+
+        mButtonPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveImage();
+            }
+        });
+
         mImageViewPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +61,20 @@ public class NuevoPost extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+    }
+
+    private void saveImage() {
+        mImagePrivder.save(NuevoPost.this, mImageFile).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(NuevoPost.this, "La imagen se pudo almacenar correctamente", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(NuevoPost.this, "Hubo un error al amacenar la imagen", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }

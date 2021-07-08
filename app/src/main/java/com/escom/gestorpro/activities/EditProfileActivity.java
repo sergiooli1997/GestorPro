2a92c04ecd79016d56120b7edf8c9e1ee33ff47b
sergiooli1997 <gio.lozol@outlook.com>
2021-07-08 17:15:55 -0500

    EditProfile 1.0

---
+++ app/src/main/java/com/escom/gestorpro/activities/EditProfileActivity.java
@@ -0,0 +1,217 @@
+package com.escom.gestorpro.activities;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.escom.gestorpro.R;
+import com.escom.gestorpro.utils.FileUtil;
+import com.google.android.material.textfield.TextInputEditText;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    CircleImageView mCircleImageViewBack;
+    CircleImageView mCircleImageViewProfile;
+    ImageView mImageViewCover;
+    TextInputEditText mTextInputUsername;
+    TextInputEditText mTextInputPhone;
+    Button mButtonEditProfile;
+
+    AlertDialog.Builder mBuilderSelector;
+    CharSequence options[];
+
+    private final int GALLERY_REQUEST_CODE_PROFILE = 1;
+    private final int GALLERY_REQUEST_CODE_COVER = 2;
+    private final int PHOTO_REQUEST_CODE_PROFILE = 3;
+    private final int PHOTO_REQUEST_CODE_COVER = 4;
+
+    // FOTO 1
+    String mAbsolutePhotoPath;
+    String mPhotoPath;
+    File mPhotoFile;
+
+    // FOTO 2
+    String mAbsolutePhotoPath2;
+    String mPhotoPath2;
+    File mPhotoFile2;
+
+    File mImageFile;
+    File mImageFile2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        mCircleImageViewBack = findViewById(R.id.circleImageBack);
+        mCircleImageViewProfile = findViewById(R.id.circleImageProfile);
+        mImageViewCover = findViewById(R.id.imageViewCover);
+        mTextInputUsername = findViewById(R.id.textInputUsername);
+        mTextInputPhone = findViewById(R.id.textInputPhone);
+        mButtonEditProfile = findViewById(R.id.btnEditProfile);
+
+        mBuilderSelector = new AlertDialog.Builder(this);
+        mBuilderSelector.setTitle("Selecciona una opción");
+        options = new CharSequence[] {"Imagen de galeria", "Tomar foto"};
+
+        mCircleImageViewProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectOptionImage(1);
+            }
+        });
+
+        mImageViewCover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectOptionImage(2);
+            }
+        });
+
+        mCircleImageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+
+    private void selectOptionImage(final int numberImage) {
+
+        mBuilderSelector.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (i == 0) {
+                    if (numberImage == 1) {
+                        openGallery(GALLERY_REQUEST_CODE_PROFILE);
+                    }
+                    else if (numberImage == 2) {
+                        openGallery(GALLERY_REQUEST_CODE_COVER);
+                    }
+                }
+                else if (i == 1){
+                    if (numberImage == 1) {
+                        takePhoto(PHOTO_REQUEST_CODE_PROFILE);
+                    }
+                    else if (numberImage == 2) {
+                        takePhoto(PHOTO_REQUEST_CODE_COVER);
+                    }
+                }
+            }
+        });
+
+        mBuilderSelector.show();
+
+    }
+
+    private void takePhoto(int requestCode) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createPhotoFile(requestCode);;
+            } catch(Exception e) {
+                Toast.makeText(this, "Hubo un error con el archivo " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+
+            if (photoFile != null) {
+                Uri photoUri = FileProvider.getUriForFile(EditProfileActivity.this, "com.escom.gestorpro", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, requestCode);
+            }
+        }
+    }
+
+    private File createPhotoFile(int requestCode) throws IOException {
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File photoFile = File.createTempFile(
+                new Date() + "_photo",
+                ".jpg",
+                storageDir
+        );
+        if (requestCode == PHOTO_REQUEST_CODE_PROFILE) {
+            mPhotoPath = "file:" + photoFile.getAbsolutePath();
+            mAbsolutePhotoPath = photoFile.getAbsolutePath();
+        }
+        else if (requestCode == PHOTO_REQUEST_CODE_COVER) {
+            mPhotoPath2 = "file:" + photoFile.getAbsolutePath();
+            mAbsolutePhotoPath2 = photoFile.getAbsolutePath();
+        }
+        return photoFile;
+    }
+
+    private void openGallery(int requestCode) {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, requestCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        /**
+         * SELECCION DE IMAGEN DESDE LA GALERIA
+         */
+        if (requestCode == GALLERY_REQUEST_CODE_PROFILE && resultCode == RESULT_OK) {
+            try {
+                mPhotoFile = null;
+                mImageFile = FileUtil.from(this, data.getData());
+                mCircleImageViewProfile.setImageBitmap(BitmapFactory.decodeFile(mImageFile.getAbsolutePath()));
+            } catch(Exception e) {
+                Log.d("ERROR", "Se produjo un error " + e.getMessage());
+                Toast.makeText(this, "Se produjo un error " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        if (requestCode == GALLERY_REQUEST_CODE_COVER && resultCode == RESULT_OK) {
+            try {
+                mPhotoFile2 = null;
+                mImageFile2 = FileUtil.from(this, data.getData());
+                mImageViewCover.setImageBitmap(BitmapFactory.decodeFile(mImageFile2.getAbsolutePath()));
+            } catch(Exception e) {
+                Log.d("ERROR", "Se produjo un error " + e.getMessage());
+                Toast.makeText(this, "Se produjo un error " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        /**
+         * SELECCION DE FOTOGRAFIA
+         */
+        if (requestCode == PHOTO_REQUEST_CODE_PROFILE && resultCode == RESULT_OK) {
+            mImageFile = null;
+            mPhotoFile = new File(mAbsolutePhotoPath);
+            Picasso.with(EditProfileActivity.this).load(mPhotoPath).into(mCircleImageViewProfile);
+        }
+
+        /**
+         * SELECCION DE FOTOGRAFIA
+         */
+        if (requestCode == PHOTO_REQUEST_CODE_COVER && resultCode == RESULT_OK) {
+            mImageFile2 = null;
+            mPhotoFile2 = new File(mAbsolutePhotoPath2);
+            Picasso.with(EditProfileActivity.this).load(mPhotoPath2).into(mImageViewCover);
+        }
+    }
+}
--- app/src/main/java/com/escom/gestorpro/fragments/profileFragment.java
+++ app/src/main/java/com/escom/gestorpro/fragments/profileFragment.java
@@ -1,5 +1,6 @@
 package com.escom.gestorpro.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.escom.gestorpro.R;
+import com.escom.gestorpro.activities.EditProfileActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +19,9 @@ import com.escom.gestorpro.R;
  * create an instance of this fragment.
  */
 public class profileFragment extends Fragment {
+
+    LinearLayout mLinearLayoutEditProfile;
+    View mView;
 
     // TODO: Agregar logout al action bar
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +65,21 @@ public class profileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        mView = inflater.inflate(R.layout.fragment_profile, container, false);
+        mLinearLayoutEditProfile = mView.findViewById(R.id.linearLayoutEditProfile);
+        mLinearLayoutEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToEditProfile();
+            }
+        });
+
+
+        return mView;
+    }
+
+    private void goToEditProfile() {
+        Intent intent = new Intent(getContext(), EditProfileActivity.class);
+        startActivity(intent);
     }
 }

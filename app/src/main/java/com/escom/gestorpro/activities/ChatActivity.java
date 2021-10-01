0589c0d98bf8cc4670ef884bdf4ecd8723ce6c0d
sergiooli1997 <gio.lozol@outlook.com>
2021-10-01 16:52:18 -0500

    ListarChats 2.0

--- app/src/main/java/com/escom/gestorpro/activities/ChatActivity.java
+++ app/src/main/java/com/escom/gestorpro/activities/ChatActivity.java
@@ -1,8 +1,13 @@
 package com.escom.gestorpro.activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.escom.gestorpro.R;
 import com.escom.gestorpro.models.Chat;
@@ -17,16 +22,31 @@ public class ChatActivity extends AppCompatActivity {
 
     ChatsProvider mChatsProvider;
 
+    View mActionBarView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        showCustomToolbar(R.layout.custom_chat_toolbar);
 
         mExtraIdUser1 = getIntent().getStringExtra("idUser1");
         mExtraIdUser2 = getIntent().getStringExtra("idUser2");
         mChatsProvider = new ChatsProvider();
         
         createChat();
+    }
+
+    private void showCustomToolbar(int resource) {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("");
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mActionBarView = inflater.inflate(resource, null);
+        actionBar.setCustomView(mActionBarView);
     }
 
     private void createChat() {
